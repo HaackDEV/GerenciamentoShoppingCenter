37ef9dbe985c8a90ec122e6cb6e062f3667a0a61
Ruan Haack <ruan.haack03@gmail.com>
2025-05-19 23:15:38 -0300

    Associação entre classes (Loja/Endereco/Data) e (Produto/Data)

--- Loja.java
+++ Loja.java
@@ -4,18 +4,24 @@ public class Loja {
     private String nome;
     private int quantidadeFuncionarios ;
     private double salarioBaseFuncionario;
+    private Endereco endereco;
+    private Data dataFundacao;
 
     // Métodos construtores.
-    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario) {
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao) {
         this.nome = nome;
         this.quantidadeFuncionarios = quantidadeFuncionarios;
         this.salarioBaseFuncionario = salarioBaseFuncionario;
+        this.endereco = endereco;
+        this.dataFundacao = dataFundacao;
     }
 
-    public Loja(String nome, int quantidadeFuncionarios) {
+    public Loja(String nome, int quantidadeFuncionarios, Endereco endereco, Data dataFuncacao) {
         this.nome = nome;
         this.quantidadeFuncionarios = quantidadeFuncionarios;
         this.salarioBaseFuncionario = -1;
+        this.endereco = endereco;
+        this.dataFundacao = dataFuncacao;
     }
 
     // Getters e setters, retornar e atualizar valores de atributos.
@@ -42,11 +48,28 @@ public class Loja {
     public void setSalarioBaseFuncionario(double salarioBaseFuncionario) {
         this.salarioBaseFuncionario = salarioBaseFuncionario;
     }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Data getDataFundacao() {
+        return dataFundacao;
+    }
+
+    public void setDataFundacao(Data dataFundacao) {
+        this.dataFundacao = dataFundacao;
+    }
+
     
     // Formata o toString para uma entrega mais entendível ao cliente final.
     @Override
     public String toString() {
-        return "Loja: " + nome + "\n" + "Quantidade de funcionarios: " + quantidadeFuncionarios + "\n" + "Salario base dos funcionarios: " + salarioBaseFuncionario;
+        return "Loja: " + nome + "\n" + "Quantidade de funcionarios: " + quantidadeFuncionarios + "\n" + "Salario base dos funcionarios: " + salarioBaseFuncionario + "\n" + "Endereco: " + endereco + "\n" + "Data de fundacao: " + dataFundacao;
     }
 
     // Método que calcula os gastos totais com salários.
@@ -69,5 +92,4 @@ public class Loja {
         }
     }
 
-    
 }
--- Produto.java
+++ Produto.java
@@ -3,11 +3,14 @@ public class Produto {
     // Atributos da classe Produto.
     private String nome;
     private double preco;
+    private Data dataValidade;
+
 
     // Método construtor.
-    public Produto(String nome, double preco) {
+    public Produto(String nome, double preco, Data dataValiade) {
         this.nome = nome;
         this.preco = preco;
+        this.dataValidade = dataValiade;
     }
 
     // Métodos de acesso.
@@ -26,10 +29,18 @@ public class Produto {
         public void setPreco(double preco) {
         this.preco = preco;
     }
+    
+    public Data getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(Data dataValidade) {
+        this.dataValidade = dataValidade;
+    }
 
     // Entrega o resultado de forma mais simples pro cliente.
     @Override
     public String toString() {
-        return "Produto: " + nome + "\n" + "Preço do produto: R$" + preco;
+        return "Produto: " + nome + "\n" + "Preço do produto: R$" + preco + "\n" + "Data de validade: " + dataValidade;
     }
 }
