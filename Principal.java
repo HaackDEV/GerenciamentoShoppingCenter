a78a795618d9a1e53c6a23df8c36bd5800865ded
Ruan Haack <ruan.haack03@gmail.com>
2025-05-23 00:36:48 -0300

    Teste Principal.java

--- Principal.java
+++ Principal.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+
 public class Principal {
     
 public static void main(String[] args) {
-    
+    Scanner teclado = new Scanner(System.in);
+    int opcao;
+
+    // while (true) {
+    //     System.out.println(".\t(1) CRIAR UMA LOJA\n");
+    //     System.out.println(".\t(2) CRIAR UM PRODUTO\n");
+    //     System.out.println(".\t(3) SAIR\n");
+
+    //     opcao = teclado.nextInt();
+
+    //     switch (opcao) {
+    //         case 1:
+    //             System.out.println("\n----- Digite os dados  -----\n");
+    //             break;
+    //         case 2: 
+
+    //             break;
+    //         case 3:
+    //             System.out.println("----- SAINDO DO PROGRAMA -----");
+    //             return;
+    //         default:
+    //             System.out.println("\n----- OPÇÃO INVÁLIDA -----\n");
+    //             break;
+    //     }
+    //     }
+    }
 }
 
-}
+c
